09dcae329842c8be155730c51c15e299b173fb67
JoseJorgeST <sarmientoamayajosejorge@gmail.com>
2023-11-30 15:24:20 -0500

    component

--- src/Routes/Navigation.tsx
+++ src/Routes/Navigation.tsx
@@ -1,35 +1,47 @@
-import { BrowserRouter, NavLink, Navigate } from 'react-router-dom';
-import logo from '../logo.svg'
-import { Route, Routes, Link } from 'react-router-dom';
-
-function Navigation() {
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    NavLink
+  } from 'react-router-dom';
+  
+  
+  import logo from '../logo.svg';
+  import { ShoppingPage } from '../02-components-patterns/pages/ShoppingPage'; 
+  
+  export const Navigation = () => {
     return (
-        <>
-            <BrowserRouter>
-                <div className="main-layout">
-                    <nav>
-                        <img src={logo} alt="Logo" />
-                        <ul>
-                            
-                        <li> <NavLink to="/" className={({ isActive }) => isActive ? 'nav-active' : ''}>Home</NavLink > </li>
-                        <li> <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''}>About</NavLink > </li>
-                        <li> <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>User</NavLink > </li>
-                    </ul>
-                    </nav>
-
-                    <Routes>
-                        <Route path='/' element={<h1>Home</h1>}/>
-                        <Route path='/about' element={<h1>About</h1>}/>
-                        <Route path='/users' element={<h1>Users</h1>}/>
-                        <Route path='/*' element={ <Navigate to={'/home'} replace />  }/>
-                    </Routes>
-                   
-                </div>
-
-            </BrowserRouter>
-
-        </>
-    )
-}
-
-export { Navigation }
+      <Router>
+        <div className="main-layout">
+          <nav>
+              <img src={ logo } alt="React Logo" />
+            <ul>
+              <li>
+                <NavLink to="/" activeClassName="nav-active" exact>Shopping</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+              </li>
+              <li>
+                <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+              </li>
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/about">
+              <h1>About</h1>
+            </Route>
+            <Route path="/users">
+              <h1>Users</h1>
+            </Route>
+            <Route path="/">
+              <ShoppingPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
